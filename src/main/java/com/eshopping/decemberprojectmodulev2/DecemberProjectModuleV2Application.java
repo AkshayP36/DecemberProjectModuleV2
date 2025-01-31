0a672a61080920424cd91f19707fb9e8daa9ea67
Akshay Patil <studyakshaystudy@outlook.com>
2025-01-31 14:08:04 +0800

    first commit

---
+++ src/main/java/com/eshopping/decemberprojectmodulev2/DecemberProjectModuleV2Application.java
@@ -0,0 +1,13 @@
+package com.eshopping.decemberprojectmodulev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecemberProjectModuleV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecemberProjectModuleV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/eshopping/decemberprojectmodulev2/DecemberProjectModuleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshopping.decemberprojectmodulev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecemberProjectModuleV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
